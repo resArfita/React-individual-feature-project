5f49461888e99eb7f91724842ca3512531860b28
res Arfita <hresaarfita@gmail.com>
2024-10-03 10:44:50 +0700

    Perbaiki styling dan Tambahkan link route back

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Playground from './components/Playground'
 import ChatbotUI from './components/ChatbotUI'
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
@@ -9,7 +9,8 @@ function App() {
     <>
     <div className="wrapper-mobile">
       <Routes>
-        <Route path="/" element={<Playground />} />
+      <Route path="/" />
+        <Route path="/playground" element={<Playground />} />
         <Route path="/chatbot" element={<ChatbotUI />} />
       </Routes>
     </div>
