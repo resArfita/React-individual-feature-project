1185379f533124ac7dd58d84da38eae85ffe0a62
res Arfita <hresaarfita@gmail.com>
2024-10-03 08:02:13 +0700

    setting Styles dan install Tailwind

---
+++ src/components/Playground.jsx
@@ -0,0 +1,12 @@
+const Playground = () => {
+    
+    return(
+        <>
+        <h1>Nobody told you this!</h1>
+        <p>Section ini memberikan kamu space untuk belajar terkait kumpulan mitos dan fakta and even learn English conversation interactively</p>
+        <button className="sm-btn-primary ml-14">Go to Playground</button>
+        </>
+    )
+}
+
+export default Playground
