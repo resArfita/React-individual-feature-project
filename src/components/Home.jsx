fa0df0fa0c8b6c424d62a83cb04287fa5a0e5aa3
res Arfita <hresaarfita@gmail.com>
2024-10-04 14:49:48 +0700

    Perbaiki tampilan Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,19 @@
 import { Link } from "react-router-dom"
+import logo from "../assets/logo.png"
 
 const Home = () => {
     
     return(
         <>
-        <h1 className="">Home</h1>
-        <Link to="/playground"><button className="sm-btn-primary ml-14">Go to Playground</button></Link>
+        <img src={logo} alt="" width={70} className="ml-2" />
+        <div className="my-5 grid grid-rows-2">
+            <p className="ml-3"></p>
+            <h1 className="font-medium text-lg">Playground</h1> 
+        </div>
+        <div className="border-2 border-white-500 shadow-md my-5 mx-3">
+            <p className="ml-2 mt-2">Section ini memberikan kamu space untuk belajar dan mendapatkan informasi-informasi menarik dengan interaktif</p>
+            <Link to="/playground"><button className="sm-btn-primary mx-2 my-5">Go to Playground</button></Link>
+        </div>
         </>
     )
 }
